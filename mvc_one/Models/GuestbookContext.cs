d6139797911d51228549f992b3613785fb64c921
Ilya Grebeshkov <grbeshkoff@gmail.com>
2014-10-14 21:00:15 +0300

    POCO GuestbookEntry

---
+++ mvc_one/Models/GuestbookContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace mvc_one.Models
+{
+    public class GuestbookContext : DbContext
+    {
+        public GuestbookContext()
+            : base("guestbook")
+        {
+        }
+
+        DbSet<GuestbookEntry> Entries { get; set; }
+    }
+}
